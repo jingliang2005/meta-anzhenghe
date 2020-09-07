4ded0e9e2f66270e4b7da8ad64d623b6adf93245
liujl-msia68-build <jingliang2005@hotmail.com>
2020-09-07 11:03:02 +0800

    test sync metas ok

--- scripts/sync_metas.sh
+++ scripts/sync_metas.sh
@@ -11,7 +11,7 @@ echo $PWD
 function cloneOrPull {
     if [ ! -d "$2" ]
     then
-        git clone $1 $2 -b dunfell --depth 1
+        git clone $1 $2 -b $3 --depth 1
     else
         git -C $2 clean -dfx
         git -C $2 reset --hard
@@ -19,12 +19,13 @@ function cloneOrPull {
     fi
 } 
 
-cloneOrPull git://git.yoctoproject.org/meta-freescale meta-freescale
- cloneOrPull  git://github.com/beagleboard/meta-beagleboard meta-beagleboard
- cloneOrPull  https://github.com/meta-qt5/meta-qt5.git  meta-qt5
- cloneOrPull  git://git.yoctoproject.org/meta-raspberrypi meta-raspberry
- cloneOrPull  https://github.com/kraj/meta-altera.git meta-altera
- cloneOrPull  git://git.yoctoproject.org/meta-ti meta-ti
-  cloneOrPull  git://git.yoctoproject.org/meta-arm meta-arm
-  cloneOrPull  git@github.com:openembedded/meta-openembedded.git meta-openembedded
+BRANCH="dunfell"
+cloneOrPull  git://git.yoctoproject.org/meta-freescale meta-freescale $BRANCH
+cloneOrPull  git://github.com/beagleboard/meta-beagleboard meta-beagleboard master
+cloneOrPull  https://github.com/meta-qt5/meta-qt5.git  meta-qt5 $BRANCH
+cloneOrPull  git://git.yoctoproject.org/meta-raspberrypi meta-raspberry $BRANCH
+cloneOrPull  https://github.com/kraj/meta-altera.git meta-altera master
+cloneOrPull  git://git.yoctoproject.org/meta-ti meta-ti $BRANCH
+cloneOrPull  git://git.yoctoproject.org/meta-arm meta-arm $BRANCH
+cloneOrPull  git@github.com:openembedded/meta-openembedded.git meta-openembedded $BRANCH
  
